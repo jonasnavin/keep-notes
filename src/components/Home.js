3fd5bd25c7e5834e50454c696c132bedeed1800e
jonasnavin <jonasnavin@gmail.com>
2024-11-24 02:35:29 +0530

    Styles updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,67 +22,59 @@ const Home = () => {
 
     return (
         <>
-        <SearchNote />
-        {
-            finalNotes.length ? (
-                <section className='notes-section'>
-                    {
-                        finalNotes.map(note => (
-                            <article
-                                key={note.id}
-                                className='notes'
-                            >
-                                <div className="content-section">
-                                    <Link to={`/note/${note.id}`}>
-                                        <h3 className='note-title'>{note.title}</h3>
-                                        <p className='note-body'>{note.body}</p>
-                                    </Link>
-                                </div>
-                                <div className='bottom-section'>
-                                    <div>
-                                        <p style={{ fontSize: '12px' }}>{note.dateTime}</p>
+            <SearchNote />
+            {
+                finalNotes.length ? (
+                    <section className='notes-section'>
+                        {
+                            finalNotes.map(note => (
+                                <article
+                                    key={note.id}
+                                    className='notes'
+                                >
+                                    <div className="content-section">
+                                        <Link to={`/note/${note.id}`}>
+                                            <h3 className='note-title'>{note.title}</h3>
+                                            <p className='note-body'>{note.body}</p>
+                                        </Link>
                                     </div>
-                                    <div className='button-section'>
-                                        <button
-                                            title='Delete Note'
-                                            className='button-groups delete-button'
-                                            onClick={() => handleDelete(note.id)}
-                                        >
-                                            <TbTrashXFilled />
-                                        </button>
-                                        <button
-                                            title='Archive Note'
-                                            className='button-groups archive-button'
-                                            onClick={() => handleArchives(note.id)}
-                                        >
-                                            <MdArchive />
-                                        </button>
-
-                                        {/* <button
-                                            className='button-groups edit-button'
-                                            title='Edit Note'>
-                                            <Link to={`/edit-note/${note.id}`}>
-                                                <MdEdit />
-                                            </Link>
-                                        </button> */}
+                                    <div className='bottom-section'>
+                                        <div>
+                                            <p style={{ fontSize: '12px' }}>{note.dateTime}</p>
+                                        </div>
+                                        <div className='button-section'>
+                                            <button
+                                                title='Delete Note'
+                                                className='button-groups delete-button'
+                                                onClick={() => handleDelete(note.id)}
+                                            >
+                                                <TbTrashXFilled />
+                                            </button>
+                                            <button
+                                                title='Archive Note'
+                                                className='button-groups archive-button'
+                                                onClick={() => handleArchives(note.id)}
+                                            >
+                                                <MdArchive />
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
-                            </article>
-                        ))
-                    }
-                </section>
-            ) : !notes.length && !search ? (
-                <section className='empty-section'>
-                    <SiGoogledocs className='svg' />
-                    <p className='svg-hint'>Notes you add appear here.</p>
-                </section>
-            ) : !finalNotes.length ? (
-                <section className='empty-section'>
-                    <SiGoogledocs className='svg' />
-                    <p className='svg-hint'>Search not found</p>
-                </section>
-            ) : null
-        }</>
+                                </article>
+                            ))
+                        }
+                    </section>
+                ) : !notes.length && !search ? (
+                    <section className='empty-section'>
+                        <SiGoogledocs className='svg' />
+                        <p className='svg-hint'>Notes you add appear here.</p>
+                    </section>
+                ) : !finalNotes.length ? (
+                    <section className='empty-section'>
+                        <SiGoogledocs className='svg' />
+                        <p className='svg-hint'>Search not found</p>
+                    </section>
+                ) : null
+            }</>
     )
 }
 
